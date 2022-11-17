c94d7b2da7cb1ce36803673a48c0e34b7feb1ad0
theliamdavidson <misterliamd@gmail.com>
2022-11-17 10:41:23 -0800

    finished test

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QPushButton
 
-
+def function():
+    print("This function has been called")
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     # ...
+    button = QPushButton("FUNCTION CALL")
+    button.clicked.connect(function)
+    button.show()
     sys.exit(app.exec())
